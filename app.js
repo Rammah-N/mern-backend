9a5f2a758b548070467485db31c449a8b789118d
Rammah <ramahnore@gmail.com>
2023-07-04 01:59:54 +0400

    add cwd to multer

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use(
+	"/uploads/images",
+	express.static(path.join(process.cwd, "uploads/images"))
+);
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
